b87215dccd810dd40b488a1c13e5f3d5c4c2ac1a
Vyacheslav Sedov <schematronic@gmail.com>
2015-09-23 07:14:18 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sla/myapplication/MainActivity.java
@@ -0,0 +1,165 @@
+package com.example.sla.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+import android.content.Intent;
+import android.util.Log;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+public class MainActivity extends Activity {
+
+    Button btnLedOn, btnLedOff;
+
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+
+    private OutputStream outStream = null;
+
+    private static String address = "20:15:05:29:70:74";
+
+    private static final int REQUEST_ENABLE_BT = 3;
+
+    // INHR Well known SPP UUID
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLedOn = (Button) findViewById(R.id.btnLedOn);
+        btnLedOff = (Button) findViewById(R.id.btnLedOff);
+        btnLedOn.setEnabled(false);
+        btnLedOff.setEnabled(false);
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        checkBTState();
+
+        //Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+
+        connectToDevice(address);
+
+//    if (pairedDevices.size() > 0) {
+//       for (BluetoothDevice device : pairedDevices) {
+//
+//        String deviceBTName = device.getName();
+//        text.setText(text.getText() + " Device: " + deviceBTName);
+//        if (deviceBTName.equals("HC-06")){
+//        	address = device.getAddress();
+//        }
+//       }
+//   }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void btnLedOn(View v) {
+        sendData("1");
+        Toast msg = Toast.makeText(getBaseContext(), "LED is ON", Toast.LENGTH_SHORT);
+        msg.show();
+    }
+
+    public void btnLedOff(View v) {
+        sendData("0");
+        Toast msg = Toast.makeText(getBaseContext(), "LED is OFF", Toast.LENGTH_SHORT);
+        msg.show();
+    }
+
+    private void errorExit(String title, String message) {
+        Toast msg = Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_SHORT);
+        msg.show();
+        finish();
+    }
+
+    private void checkBTState() {
+        // Check for Bluetooth support and then check to make sure it is turned on
+
+        // Emulator doesn't support Bluetooth and will return null
+        if (btAdapter == null) {
+            errorExit("Fatal Error", "Bluetooth Not supported. Aborting.");
+        } else {
+            if (btAdapter.isEnabled()) {
+                //   Log.d(TAG, "...Bluetooth is enabled...");
+            } else {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(btAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    public void connectToDevice(String adr) {
+        super.onResume();
+
+        //enable buttons once connection established.
+        btnLedOn.setEnabled(true);
+        btnLedOff.setEnabled(true);
+
+
+        // Set up a pointer to the remote node using it's address.
+        BluetoothDevice device = btAdapter.getRemoteDevice(adr);
+
+        // Two things are needed to make a connection:
+        //   A MAC address, which we got above.
+        //   A Service ID or UUID.  In this case we are using the
+        //     UUID for SPP.
+        try {
+            btSocket = device.createRfcommSocketToServiceRecord(MY_UUID);
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and socket create failed: " + e.getMessage() + ".");
+        }
+
+        // Discovery is resource intensive.  Make sure it isn't going on
+        // when you attempt to connect and pass your message.
+        btAdapter.cancelDiscovery();
+
+        // Establish the connection.  This will block until it connects.
+        try {
+            btSocket.connect();
+        } catch (IOException e) {
+            try {
+                btSocket.close();
+            } catch (IOException e2) {
+                errorExit("Fatal Error", "In onResume() and unable to close socket during connection failure" + e2.getMessage() + ".");
+            }
+        }
+
+        // Create a data stream so we can talk to server.
+        try {
+            outStream = btSocket.getOutputStream();
+        } catch (IOException e) {
+            errorExit("Fatal Error", "In onResume() and output stream creation failed:" + e.getMessage() + ".");
+        }
+    }
+
+
+    private void sendData(String message) {
+        byte[] msgBuffer = message.getBytes();
+        try {
+            outStream.write(msgBuffer);
+        } catch (IOException e) {
+            String msg = "In onResume() and an exception occurred during write: " + e.getMessage();
+            errorExit("Fatal Error", msg);
+        }
+    }
+}
